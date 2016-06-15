cf4475ae1fac6717b01312be464bde1fe6c0e157
Sacharomyces <simson.kenny@gmail.com>
2016-06-15 22:28:10 +0200

    no message

--- Zadanie str 390 (Go Fish!)/Zadanie str 390 (Go Fish!)/Deck.cs
+++ Zadanie str 390 (Go Fish!)/Zadanie str 390 (Go Fish!)/Deck.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Zadanie_str_382__decks_exchange_
+namespace Zadanie_str_390__Go_Fish__
 {
     class Deck
     {
--- Zadanie str 390 (Go Fish!)/Zadanie str 390 (Go Fish!)/Game.cs
+++ Zadanie str 390 (Go Fish!)/Zadanie str 390 (Go Fish!)/Game.cs
@@ -8,5 +8,13 @@ namespace Zadanie_str_390__Go_Fish__
 {
     class Game
     {
+        public Game(string PlayerName, List<string> opponents, string log )
+        {
+            playerName = PlayerName;
+
+        }
+        private string playerName;
+        private string log;
+
     }
 }
