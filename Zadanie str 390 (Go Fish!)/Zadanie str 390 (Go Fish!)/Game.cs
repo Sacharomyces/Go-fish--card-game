1d9e6210c840c75597d80cc91bb29a3cf1bfe374
Sacharomyces <simson.kenny@gmail.com>
2016-06-15 22:27:34 +0200

    Added player and game classes

---
+++ Zadanie str 390 (Go Fish!)/Zadanie str 390 (Go Fish!)/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_str_390__Go_Fish__
+{
+    class Game
+    {
+    }
+}
---
+++ Zadanie str 390 (Go Fish!)/Zadanie str 390 (Go Fish!)/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_str_390__Go_Fish__
+{
+    class Player
+    {
+        private string name;
+        public string Name { get { return name; } }
+        private Random random;
+        private  
+    }
+}
